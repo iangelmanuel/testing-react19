efc53a3f3c65aeaef0f9c7a61b9ff1ee830c3237
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-22 17:47:51 -0500

    feat: Add UseTransitions component

---
+++ src/components/UseTransitions.jsx
@@ -0,0 +1,42 @@
+import { useState, useTransition } from 'react'
+
+const getFetchData = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/photos')
+  const data = await response.json()
+  return data
+}
+
+export const UseTransitions = () => {
+  const [data, setData] = useState([])
+  const [isPending, startTransition] = useTransition()
+
+  const handleClick = () => {
+    startTransition(async () => {
+      const error = await getFetchData()
+
+      if (error) {
+        console.log(error)
+        return
+      }
+
+      setData(data)
+    })
+  }
+
+  const checkLabel = isPending ? 'Cargando...' : 'Extraer datos'
+
+  return (
+    <>
+      <h5>{checkLabel}</h5>
+      {data.map((photo) => (
+        <div key={photo.id}>
+          <img src={photo.thumbnailUrl} alt={photo.title} />
+          <p>{photo.title}</p>
+        </div>
+      ))}
+      <button onClick={handleClick} disabled={isPending}>
+        Extraer datos
+      </button>
+    </>
+  )
+}
