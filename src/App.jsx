8de10492af0584be7f1e328aad58568460e7c6fa
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-23 05:37:31 -0500

    refactor: Adjust image size in UseActionState component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { version } from 'react'
 import { Seo } from './components/Seo'
+
+import { UseTransitionExample } from './components/examples/UseTransitionExample'
 import { UseTransitions } from './components/UseTransitions'
+
+import { UseActionStateExample } from './components/examples/UseActionStateExample'
 import { UseActionState } from './components/UseActionState'
+
+import { UseFormStatusExample } from './components/examples/UseFormStatusExample'
 import { UseFormStatus } from './components/UseFormStatus'
+
+import { UseExample } from './components/examples/UseExamples'
+import { UseExampleTwoExample } from './components/examples/UseExampleTwoExample'
+
+// import { Use } from './components/Use'
 import { Github } from './svg/Github'
 import { React } from './svg/React'
 
@@ -80,7 +91,7 @@ export default function App() {
           marginBottom: '10rem',
         }}
       >
-        <h2 style={{ color: 'purple', margin: 0 }}>Uso de useTransitions</h2>
+        <h2 style={{ color: 'purple', margin: 0 }}>Uso de useTransitions()</h2>
         <h4 style={{ margin: 0 }}>Explicación:</h4>
         <p style={{ color: 'gray', textAlign: 'center', margin: 0 }}>
           El useTransitions se encarga de manejar funciones asincronas con un
@@ -91,10 +102,11 @@ export default function App() {
             target="_blank"
             rel="noreferrer"
           >
-            useTransitions.jsx
+            UseTransitions.jsx
           </a>{' '}
           para ver el código
         </p>
+        <UseTransitionExample />
         <UseTransitions />
       </div>
 
@@ -110,7 +122,7 @@ export default function App() {
           marginBottom: '10rem',
         }}
       >
-        <h2 style={{ color: 'purple', margin: 0 }}>Uso de useActionState</h2>
+        <h2 style={{ color: 'purple', margin: 0 }}>Uso de useActionState()</h2>
         <h4 style={{ margin: 0 }}>Explicación:</h4>
         <p style={{ color: 'gray', textAlign: 'center' }}>
           El useActionState se encarga de manejar todo el estado del formulario,
@@ -122,10 +134,11 @@ export default function App() {
             target="_blank"
             rel="noreferrer"
           >
-            useActionState.jsx
+            UseActionState.jsx
           </a>{' '}
           para ver el código
         </p>
+        <UseActionStateExample />
         <UseActionState />
       </div>
 
@@ -141,7 +154,7 @@ export default function App() {
           marginBottom: '10rem',
         }}
       >
-        <h2 style={{ color: 'purple', margin: 0 }}>Uso de useActionStatus</h2>
+        <h2 style={{ color: 'purple', margin: 0 }}>Uso de useActionStatus()</h2>
         <h4 style={{ margin: 0 }}>Explicación:</h4>
         <p style={{ color: 'gray', textAlign: 'center' }}>
           El useActionStatus se utiliza para el manejo de estados de carga de un
@@ -158,7 +171,85 @@ export default function App() {
           </a>{' '}
           para ver el código
         </p>
+        <UseFormStatusExample />
         <UseFormStatus />
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 20,
+          maxWidth: 800,
+          margin: 'auto',
+          marginBottom: '10rem',
+        }}
+      >
+        <h2 style={{ color: 'purple', margin: 0 }}>Uso de use()</h2>
+        <h4 style={{ margin: 0 }}>Explicación:</h4>
+        <p style={{ color: 'gray', textAlign: 'center' }}>
+          La nueva API de React llamada{' '}
+          <strong style={{ color: 'white' }}>use</strong> se utiliza para
+          manejar la carga de un componente de manera asincrona, es decir, el
+          componente se monta de manera asincrona utilizando dentro la API nueva
+          de react{' '}
+          <strong style={{ color: 'white' }}>use(Función Asíncrona)</strong>,
+          luego debemos envolver nuestro componente asíncrono con el componente{' '}
+          <strong style={{ color: 'white' }}>{'<Suspense />'}</strong> para
+          manejar la carga de manera correcta con su respectivo{' '}
+          <strong>fallback</strong> como atributo del Suspense.{' '}
+          <a
+            href="https://github.com/iAngelManuel/testing-react19/blob/main/src/components/UseFormStatus.jsx"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Use.jsx
+          </a>{' '}
+          para ver el código
+        </p>
+        {/* <Use /> */}
+        <UseExample />
+        <UseExampleTwoExample />
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 20,
+          maxWidth: 800,
+          margin: 'auto',
+          marginBottom: '10rem',
+        }}
+      >
+        <h2 style={{ color: 'purple', margin: 0 }}>Uso de useOptimistic()</h2>
+        <h4 style={{ margin: 0 }}>Explicación:</h4>
+        <p style={{ color: 'gray', textAlign: 'center' }}>
+          La nueva API de React llamada{' '}
+          <strong style={{ color: 'white' }}>use</strong> se utiliza para
+          manejar la carga de un componente de manera asincrona, es decir, el
+          componente se monta de manera asincrona utilizando dentro la API nueva
+          de react{' '}
+          <strong style={{ color: 'white' }}>use(Función Asíncrona)</strong>,
+          luego debemos envolver nuestro componente asíncrono con el componente{' '}
+          <strong style={{ color: 'white' }}>{'<Suspense />'}</strong> para
+          manejar la carga de manera correcta con su respectivo{' '}
+          <strong>fallback</strong> como atributo del Suspense.{' '}
+          <a
+            href="https://github.com/iAngelManuel/testing-react19/blob/main/src/components/UseFormStatus.jsx"
+            target="_blank"
+            rel="noreferrer"
+          >
+            useOptimistic.jsx
+          </a>{' '}
+          para ver el código
+        </p>
+        {/* <Use /> */}
+        <UseExample />
       </div>
     </>
   )
